94ed2f4f77248aa7f023973fbf120de539585a06
Paulina Brajovic <pollybrajovic@hotmail.com>
2024-10-13 00:40:58 -0300

    Subida inicial

---
+++ README.md
@@ -0,0 +1,11 @@
+<h1 align="left"> Cafetería Nanacao - Node </h1>
+
+<h3 align="left">
+Para ejecutar scripts de node desde la terminal, se ejecutar "npm i", luego "npm run test".
+</h4>
+
+```node
+npm i
+
+npm run test
+```
---
+++ tests/server.spec.js
@@ -0,0 +1,38 @@
+const request = require("supertest");
+const server = require("../index");
+
+describe("Operaciones CRUD de cafes", () => {
+
+  it("GET/cafes status 200 y tipo de dato arreglo con al menos un objeto", async () => {
+    const response = await request(server).get("/cafes");
+    expect(response.status).toBe(200);
+    expect(Array.isArray(response.body)).toBe(true);
+    expect(response.body.length).toBeGreaterThan(0);
+    expect(typeof response.body[0]).toBe('object'); 
+  });
+
+  it('DELETE/cafes status 404 para id que no existe', async () => {
+    const response = await request(server).delete('/cafes/10').set('Authorization', 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQHZpZGFzYW5hLmNvbSIsImlhdCI6MTcyODE3NTg5Nn0.b1-GOPqEGTpExBQeRBPh0KIg8tR03mMPnqF-b8qwTu8');;
+    console.log('Test delete status',response.status);
+     expect(response.status).toBe(404);
+  });
+
+  it("POST/cafes status 201 al agregar nuevo cafe", async () => {
+    const nuevoCafe = {
+        id: 5, 
+        nombre: "Expresso"
+      };
+    const response = await request(server).post("/cafes").send(nuevoCafe);
+    expect(response.status).toBe(201);
+  });
+
+  it("PUT/cafes status 400 si id params es distinto al id payload", async () => {
+    const cafe = {
+        id: 1, 
+        nombre: "Cortado"
+      };
+    const response = await request(server).put("/cafes/3").send(cafe);
+    expect(response.status).toBe(400);
+  });
+
+});
